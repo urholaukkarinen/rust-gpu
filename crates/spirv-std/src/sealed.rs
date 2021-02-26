a67abf3335b424fe4f3a0d4bb1709a943fad9299
Erin Power <erin.power@embark-studios.com>
2021-02-26 08:04:02 +0100

    Revert to glam 0.11

--- crates/spirv-std/src/sealed.rs
+++ crates/spirv-std/src/sealed.rs
@@ -12,16 +12,7 @@ impl Sealed for i8 {}
 impl Sealed for i16 {}
 impl Sealed for i32 {}
 impl Sealed for i64 {}
-impl Sealed for glam::BVec2 {}
-impl Sealed for glam::BVec3 {}
-impl Sealed for glam::BVec4 {}
 impl Sealed for glam::Vec2 {}
 impl Sealed for glam::Vec3 {}
 impl Sealed for glam::Vec3A {}
 impl Sealed for glam::Vec4 {}
-impl Sealed for glam::UVec2 {}
-impl Sealed for glam::UVec3 {}
-impl Sealed for glam::UVec4 {}
-impl Sealed for glam::IVec2 {}
-impl Sealed for glam::IVec3 {}
-impl Sealed for glam::IVec4 {}
